01fa1a0fa1de2a5b3d5bff19df1ee48ef92abbbb
patsakornakanit-hash <patsakorn.akanit@gmail.com>
2026-04-14 08:23:41 +0700

    DataManager

--- Course.java
+++ Course.java
@@ -1,19 +1,62 @@
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
-public class Course {
+public class Course implements Subject, Serializable {
     private String courseName;
+    private String courseID;
     private List<Student> enrolledStudents;
+    private Map<Student, String> grades;
+    private List<Observer> observers;
+    private GradingStrategy gradingStrategy;
 
-    public Course(String courseName) {
+    public Course(String courseName,String courseID,GradingStrategy gradingStrategy) {
         this.courseName = courseName;
+        this.courseID = courseID;
+        this.gradingStrategy = gradingStrategy;
         this.enrolledStudents = new ArrayList<>();
+        this.grades = new HashMap<>();
+        this.observers = new ArrayList<>();
     }
 
     public String getCourseName() { return courseName; }
+    public String getCourseID() { return courseID; }
     public List<Student> getEnrolledStudents() { return enrolledStudents; }
+    public Map<Student, String> getgrades() {return grades;};
 
     public void addStudent(Student student) {
-        this.enrolledStudents.add(student);
+        if (!enrolledStudents.contains(student)) {
+            this.enrolledStudents.add(student);
+            this.registerObserver(student);
+        }
     }
+
+    public void gradeStudent(Student student, double rawScore) {
+        if (enrolledStudents.contains(student)) {
+            String finalGrade = gradingStrategy.calculateGrade(rawScore);
+            grades.put(student, finalGrade);
+            notifyObservers("final grade out");
+        }
+        else {
+            System.out.println("Error: Student not enrolled.");
+        }
+    }
+    @Override
+    public void registerObserver(Observer o) {
+        if (!observers.contains(o)) observers.add(o);
+    }
+
+    @Override
+    public void removeObserver(Observer o) {
+        observers.remove(o);
+    }
+
+    @Override
+    public void notifyObservers(String message) {
+        for (Observer o : observers) {
+            o.update(message);
+        }
+}
 }

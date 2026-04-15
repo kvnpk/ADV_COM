128b1e266df11bcb6c108437472b804ae9750f55
kvnpk <kevincoolmaanpkc@gmail.com>
2026-04-15 19:05:18 +0700

    Change Grade Criterias

--- GradingCriteria.java
+++ GradingCriteria.java
@@ -2,27 +2,28 @@ import java.io.Serializable;
 
 public class GradingCriteria implements Serializable {
     private double minA;
+    private double minBplus;
     private double minB;
+    private double minCplus;
     private double minC;
+    private double minDplus;
     private double minD;
 
-    public GradingCriteria(double minA, double minB, double minC,double minD) {
+    public GradingCriteria(double minA, double minBplus, double minB, double minCplus, double minC, double minDplus, double minD) {
         this.minA = minA;
+        this.minBplus = minBplus;
         this.minB = minB;
+        this.minCplus = minCplus;
         this.minC = minC;
+        this.minDplus = minDplus;
         this.minD = minD;
     }
 
-    public double getminA() {
-        return minA;
-    }
-    public double getminB() {
-        return minB;
-    }
-    public double getminC() {
-        return minC;
-    }
-    public double getminD() {
-        return minD;
-    }
+    public double getminA() { return minA; }
+    public double getminBplus() { return minBplus; }
+    public double getminB() { return minB; }
+    public double getminCplus() { return minCplus; }
+    public double getminC() { return minC; }
+    public double getminDplus() { return minDplus; }
+    public double getminD() { return minD; }
 }

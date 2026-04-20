ef2f75dfa762b9c166a790de58e8c7ffa09f651d
patsakornakanit-hash <patsakorn.akanit@gmail.com>
2026-04-20 20:19:01 +0700

    edit

--- User.java
+++ User.java
@@ -1,5 +1,5 @@
 import java.io.Serializable;
-import java.util.Objects; // Don't forget this new import!
+import java.util.Objects; 
 
 public abstract class User implements Serializable {
     private static final long serialVersionUID = 1L;
@@ -18,13 +18,11 @@ public abstract class User implements Serializable {
     public String getId() { return id; }
     public void setId(String id) { this.id = id; }
 
-    // --- NEW: Teach Java how to compare Users ---
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        // Two users are the exact same if they have the same ID
         return Objects.equals(id, user.id);
     }
 
